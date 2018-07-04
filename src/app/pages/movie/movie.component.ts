a26818590168ef2a544627ca24bb48e3944c7a76
Paolo Tesser <p.tesser921@gmail.com>
2018-07-04 22:39:58 +0200

    (movie) - Separate model from service

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService, Movie } from '../../services/movie.service';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-movie',
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -4,27 +4,7 @@ import 'rxjs/add/operator/map';
 
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
-
-export interface MovieList {
-  page: number;
-  total_results: number;
-  total_pages: number;
-  results: Movie[];
-}
-
-export interface Movie {
-  id: number;
-  title: string;
-  overview: string;
-  original_languages: string;
-  poster_path: string;
-  genres_ids: string[];
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-
-  image: string;
-}
+import { MovieList, Movie } from '../models/movie';
 
 @Injectable()
 export class MovieService {
