a847bb06d2f36715d2c94559622cea6e094396d2
Paolo Tesser <p.tesser921@gmail.com>
2018-08-30 23:06:10 +0200

    Remove all files

--- src/app/pages/documentation/documentation-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { routes } from './documentation.component';
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class DocumentationRoutingModule { }
